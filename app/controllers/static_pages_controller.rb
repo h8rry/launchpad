2ac32438d3be6dd5a1ef8c23465ccb827bf2f2f0
Harry Chen <harrychenca@gmail.com>
2013-05-18 15:58:05 -0700

    3:58

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,25 @@
 class StaticPagesController < ApplicationController
 	def home
 
-		require "open-uri"
-		require 'net/http'
+
 
 	end
 
 	def fb
-		require "open-uri"
-		require 'net/http'
 
-		url = "https://graph.facebook.com/oauth/access_token?client_id=191338297661848&redirect_uri=http://launchp.herokuapp.com/fb&client_secret=e218f0abdb012a0fc8c49e135b8af61d&code=" + params[:code] 
+require "net/https"
+require "uri"
 
-		redirect_to url
+url = "https://graph.facebook.com/oauth/access_token?client_id=191338297661848&redirect_uri=http://launchp.herokuapp.com/fb&client_secret=e218f0abdb012a0fc8c49e135b8af61d&code=" + params[:code] 
+
+uri = URI.parse(url)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(uri.request_uri)
+
+@response = http.request(request)
 
 	end
 end
