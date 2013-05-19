102849e192b9a7e89a288739355de787a0e59a04
bekt <bekt17@gmail.com>
2013-05-18 20:07:23 -0700

    Hack hard

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -2,23 +2,35 @@ class DevController < ApplicationController
 	@@app_id = 191338297661848
 	@@app_secret = "e218f0abdb012a0fc8c49e135b8af61d"
 	@@fb_redirect_url = "http://launchp.herokuapp.com/dev/resp"
-		
-	def index
-		@url = "https://www.facebook.com/dialog/oauth?client_id=#{@@app_id}&redirect_uri=#{@@fb_redirect_url}"
-	end
 
 	def resp
 		code = params[:code]
-		url = "https://graph.facebook.com/oauth/access_token?client_id=#{@@app_id}&client_secret=#{@@app_secret}&code=#{code}&redirect_uri=#{@@fb_redirect_url}"
+		#TODO: validation (code)
 
-		@access_token = makeHttpsGetRequest url
+		# Exchange code for an access token
+		url = "https://graph.facebook.com/oauth/access_token?client_id=#{@@app_id}&client_secret=#{@@app_secret}&code=#{code}&redirect_uri=#{@@fb_redirect_url}"
+		token = makeHttpsGetRequest url
+		token = token[token.index("access_token")+13, token.index("expires")-2]
+		#TODO: validation (token)
+		
+		# Get stuff about the user
+		url = "https://graph.facebook.com/me?access_token=#{token}"
+		@result = makeHttpsGetRequest url		
 	end
 	
-	def disp
-		token = params[:token]
-		url = "https://graph.facebook.com/me?access_token=#{token}"
-		
-		@result = makeHttpsGetRequest url
+	# Redirect to Facebook sign up/in page with our credentials.
+	# Reponse url: our url to handle Facebook's callback
+	def facebook
+		url = "https://www.facebook.com/dialog/oauth?client_id=#{@@app_id}&redirect_uri=#{@@fb_redirect_url}"
+		redirect_to url
+	end
+	
+	def user
+	
+	end
+	
+	def userCallback
+	
 	end
 	
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Launchpad::Application.routes.draw do
 
   resources :users
 
-  get "dev/index"
-  
   get "dev/resp"
   
-  get "dev/disp"
- 
+  # Redirect only
+  get "dev/facebook"
+  get "dev/twitter"
+  get "dev/google"
+  
+  get "dev/user"
+  get "dev/userCallback"
+  
   root to: "static_pages#home"
 
   match '/fb', to: 'static_pages#fb'
