eccf0cf37a4d496679c2f4fefb2b53c8cf5e20b8
bekt <bekt17@gmail.com>
2013-05-19 02:41:45 -0700

    1

--- app/controllers/linkedin_controller.rb
+++ app/controllers/linkedin_controller.rb
@@ -13,13 +13,21 @@ class LinkedinController < ApplicationController
 
   def resp
     code = params[:code]
-
     token = getToken code
-    @user = token
+    @user = getUser token
   end
 
   def getToken(code)
+    require 'json'
+
     url = "https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&client_id=#{@@app_key}&client_secret=#{@@secret_key}&redirect_uri=#{@@redirect_uri}&code=#{code}"
+    token = DevController.makeHttpsGetRequest url
+    json = JSON.parse token
+    json["access_token"]
+  end
+
+  def getUser(token)
+    url = "https://api.linkedin.com/v1/people/~?oauth2_access_token=#{token}"
     DevController.makeHttpsGetRequest url
   end
 
